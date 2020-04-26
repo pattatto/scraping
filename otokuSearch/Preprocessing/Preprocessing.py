b68d8a018fa9b1497b5d423356ae656278059324
pattatto <pattatto114514@gmail.com>
2020-04-26 20:26:54 +0900

    Add:立地情報からバス停、バス停までの時間、駅までの車の時間を分離。バス停と建物種別のLabel encoding追加。出力dfにbuildingを追加。
    特徴量作成に必要になったため追加した。
    
    参考
    https://qiita.com/haraso_1130/items/8ea9ba66f9d5f0fc2157#%E7%89%B9%E5%BE%B4%E9%87%8F%E4%BD%9C%E6%88%90

--- otokuSearch/Preprocessing/Preprocessing.py
+++ otokuSearch/Preprocessing/Preprocessing.py
@@ -64,9 +64,9 @@ df['賃料料'] = df['賃料料'] * 10000
 df['敷金'] = df['敷金'] * 10000
 df['礼金'] = df['礼金'] * 10000
 
-df['徒歩1'] = pd.to_numeric(df['徒歩1'])
-df['徒歩2'] = pd.to_numeric(df['徒歩2'])
-df['徒歩3'] = pd.to_numeric(df['徒歩3'])
+df['駅徒歩1'] = pd.to_numeric(df['駅徒歩1'])
+df['駅徒歩2'] = pd.to_numeric(df['駅徒歩2'])
+df['駅徒歩3'] = pd.to_numeric(df['駅徒歩3'])
 
 splitted8 = df['階層'].str.split('-', expand=True)
 splitted8.columns = ['階1', '階2']
@@ -125,23 +125,71 @@ splitted9.columns = ['市町村']
 #splitted9['区'] = splitted9['区'].str.replace('東京都','')
 df = pd.concat([df, splitted9], axis=1)
 
-df_for_search = df.copy()
+splitted10 = df['駅1'].str.split(' バス', expand=True)
+splitted10.columns = ['駅1', 'バス1']
+splitted11 = df['駅2'].str.split(' バス', expand=True)
+splitted11.columns = ['駅2', 'バス2']
+splitted12 = df['駅3'].str.split(' バス', expand=True)
+splitted12.columns = ['駅3', 'バス3']
 
-df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村']] = df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村']].fillna("NAN")
+splitted13 = splitted10['バス1'].str.split('分 \(バス停\)', expand=True)
+splitted13.columns = ['バス時間1', 'バス停1']
+splitted14 = splitted11['バス2'].str.split('分 \(バス停\)', expand=True)
+splitted14.columns = ['バス時間2', 'バス停2']
+splitted15 = splitted12['バス3'].str.split('分 \(バス停\)', expand=True)
+splitted15.columns = ['バス時間3', 'バス停3']
+
+splitted16 = pd.concat([splitted10, splitted11, splitted12, splitted13, splitted14, splitted15], axis=1)
+splitted16.drop(['バス1','バス2','バス3'], axis=1, inplace=True)
+
+df.drop(['駅1','駅2','駅3'], axis=1, inplace=True)
+df = pd.concat([df, splitted16], axis=1)
+
+splitted17 = df['駅1'].str.split(' 車', expand=True)
+splitted17.columns = ['駅1', '車1']
+splitted18 = df['駅2'].str.split(' 車', expand=True)
+splitted18.columns = ['駅2', '車2']
+splitted19 = df['駅3'].str.split(' 車', expand=True)
+splitted19.columns = ['駅3', '車3']
+
+splitted20 = splitted17['車1'].str.split('分', expand=True)
+splitted20.columns = ['車時間1', '車距離1']
+splitted21 = splitted18['車2'].str.split('分', expand=True)
+splitted21.columns = ['車時間2', '車距離2']
+splitted22 = splitted19['車3'].str.split('分', expand=True)
+splitted22.columns = ['車時間3', '車距離3']
+
+splitted23 = pd.concat([splitted17, splitted18, splitted19, splitted20, splitted21, splitted22], axis=1)
+splitted23.drop(['車1','車2','車3'], axis=1, inplace=True)
+
+df.drop(['駅1','駅2','駅3'], axis=1, inplace=True)
+df = pd.concat([df, splitted23], axis=1)
+
+df['車距離1'] = df['車距離1'].str.replace(u'\(', u'')
+df['車距離1'] = df['車距離1'].str.replace(u'km\)', u'')
+df['車距離2'] = df['車距離2'].str.replace(u'\(', u'')
+df['車距離2'] = df['車距離2'].str.replace(u'km\)', u'')
+df['車距離3'] = df['車距離3'].str.replace(u'\(', u'')
+df['車距離3'] = df['車距離3'].str.replace(u'km\)', u'')
+
+df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村', 'バス停1', 'バス停2', 'バス停3']] = df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村', 'バス停1', 'バス停2', 'バス停3']].fillna("NAN")
+df[['バス時間1','バス時間2','バス時間3',]] = df[['バス時間1','バス時間2','バス時間3']].fillna(0)#欠損値あると特徴量の計算でエラーがでるため０に置換
 
 oe = preprocessing.OrdinalEncoder()
-df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村']] = oe.fit_transform(df[['路線1','路線2','路線3', '駅1', '駅2','駅3','市町村']].values)
+df[['建物種別', '路線1','路線2','路線3', '駅1', '駅2','駅3','市町村', 'バス停1', 'バス停2', 'バス停3']] = oe.fit_transform(df[['建物種別', '路線1','路線2','路線3', '駅1', '駅2','駅3','市町村', 'バス停1', 'バス停2', 'バス停3']].values)
 
 df['賃料料+管理費'] = df['賃料料'] + df['管理費']
+
+df_for_search = df.copy()
 
 #上限価格を設定
 df = df[df['賃料料+管理費'] < 300000]
 
-df = df[["マンション名",'賃料料+管理費', '築年数', '建物の高さ', '階1',
+df = df[["マンション名",'建物種別', '賃料料+管理費', '築年数', '建物の高さ', '階1',
        '専有面積','路線1','路線2','路線3', '駅1', '駅2','駅3','徒歩1', '徒歩2','徒歩3','間取り', '間取りDK', '間取りK', '間取りL', '間取りS',
        '市町村']]
 
-df.columns = ['name','real_rent','age', 'hight', 'level','area', 'route_1','route_2','route_3','station_1','station_2','station_3','distance_1','distance_2','distance_3','room_number','DK','K','L','S','adress']
+df.columns = ['name', 'building', 'real_rent','age', 'hight', 'level','area', 'route_1','route_2','route_3','station_1','station_2','station_3','distance_1','distance_2','distance_3','room_number','DK','K','L','S','adress']
 
 
 #pdp.ProfileReport(df)
