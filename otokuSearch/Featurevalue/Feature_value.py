8b1be0fc136c12c9ca948d6f038fcf898be56fc1
pattatto <pattatto114514@gmail.com>
2020-04-26 16:37:54 +0900

    初回登録

---
+++ otokuSearch/Featurevalue/Feature_value.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('otokuSearch/Preprocessing/Preprocessing.csv', sep='\t', encoding='utf-16')
+
+df["per_area"] = df["area"]/df["room_number"]
+df["per_real_rent"] = df["real_rent"]/df["area"]
+df["hight_level"] = df["hight"]*df["level"]
+df["area_hight_level"] = df["area"]*df["hight_level"]
+df["distance_staion_1"] = df["station_1"]*df["station_wolk_1"]+df["bus_stop1"]*df["bus_time1"]
+df["distance_staion_2"] = df["station_2"]*df["station_wolk_2"]+df["bus_stop2"]*df["bus_time2"]
+df["distance_staion_3"] = df["station_3"]*df["station_wolk_3"]+df["bus_stop3"]*df["bus_time3"]
+
+df.to_csv('otokuSearch/Featurevalue/Fettur_evalue.csv', sep = '\t', encoding='utf-16', header=True, index=False)
---
+++ otokuSearch/model/model_lightgbm.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split
